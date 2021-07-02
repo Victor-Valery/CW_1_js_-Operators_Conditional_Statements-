59a398956f54a298d48e1dd31be24a92f2a71a65
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-07-02 15:53:09 +0200

    js_studies

--- add.js
+++ add.js
@@ -1,3 +1,5 @@
+// Operators
+
 // var a = 20 + 30;
 // console.log(a);
 
@@ -18,17 +20,131 @@
 
 // *******************
 
+// Asignment Operators
+
 // var a = 20;
 // a -= 5;
 // console.log(a);
 
-// *******************
+// --------------------------
 
 // var a = 27;
 // a %= 5;
 // console.log(a);
 
 // *******************
+
+// Comparison Operators
+
+// All comparison operators return Boolean (true or false).
+
+// var a = 10;
+// console.log(a==9);
+
+// --------------------------
+
+// var a = 10;
+// console.log(a==10);
+
+// --------------------------
+
+// var a = "10";
+// console.log(a===10);
+
+// --------------------------
+
+// var a = 10;
+// console.log(a!==13);
+
+// *******************
+
+// var a = 10;
+// var b = 5;
+// console.log(a>=b && b<=20);
+
+// --------------------------
+
+// var a = 10;
+// var b = 5;
+// console.log(!(a>=b));
+
+// --------------------------
+
+// var a = 3;
+// var b = 7;
+// console.log(a>=b && b<=20);
+
+// *******************
+
+// Nullish coalescing operator
+
+// const foo = null ?? 'default string';
+// console.log(foo);
+// // expected output: "default string"
+
+// const baz = 0 ?? 42;
+// console.log(baz);
+// // expected output: 0
+
+// const nullValue = null;
+// const emptyText = ""; // falsy
+// const someNumber = 42;
+
+// const valA = nullValue ?? "default for A";
+// const valB = emptyText ?? "default for B";
+// const valC = someNumber ?? 0;
+
+// console.log(valA); // "default for A"
+// console.log(valB); // "" (as the empty string is not null or undefined)
+// console.log(valC); // 42
+
+// *******************
+
+// var x = 5;
+// var y = 2;
+
+// x = x*=y
+// console.log(x);
+
+// *******************
+
+// var x = 5;
+// var y = 2;
+
+// x*=y
+
+// console.log(x);
+
+// *******************
+
+// var a = 3;
+// var b = 2;
+
+// console.log(a*b);
+
+// x*=y;
+
+// *******************
+
+// var number = 10;
+// // number = number + 5;
+// number += 5;
+
+// *******************
+
+// var a = 100;
+// a *= 5;
+
+// console.log(a);
+
+// console.log(7 !== 10);
+
+// *******************
+// *******************
+
+// Conditional Statements
+
+
 
 
 
